5c45896e453359fd0cf8601515fcdfb7d6558b6f
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-09 23:24:42 +0100

    Add main components and css

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Header';
+import Navigation from './Navigation';
+import Page from './Page';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
-      <>
-        <div className='App'> test</div>
-      </>
+      <Router>
+        <div className='app'>
+          <header>{<Header />}</header>
+          <main>
+            <aside>{<Navigation />}</aside>
+            <section className='page'>{<Page />}</section>
+          </main>
+          <footer>{<Footer />}</footer>
+        </div>
+      </Router>
     );
   }
 }
