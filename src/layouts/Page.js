bf630d7e18ce5579b46622d9aa6ad8be0f7912aa
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-11 18:15:45 +0100

    Add pages

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import AdminPage from '../pages/AdminPage';
+import ContactPage from '../pages/ContactPage';
+import ErrorPage from '../pages/ErrorPage';
+import HomePage from '../pages/HomePage';
+import ProductPage from '../pages/ProductPage';
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <>
+      <Switch>
+        <Route path='/' exact component={HomePage} />
+        <Route path='/products' component={ProductPage} />
+        <Route path='/contact' component={ContactPage} />
+        <Route path='/admin' component={AdminPage} />
+        <Route component={ErrorPage} />
+      </Switch>
+    </>
+  );
 };
 
 export default Page;
